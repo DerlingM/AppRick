d0df3fd01cf0dc4957d27c57022d4bf4c1d280a8
DerlingM <mdmcstudios@hotmail.com>
2023-01-28 13:19:57 -0600

    first commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,8 @@ import React from "react";
 export default function About(){
     return(
         <div>
-              <h1>soy el about</h1>
-        <p> Esta es mi primera aplicacion en react</p>
+              <h1 style={{color: "white" } }>Mario Derling</h1>
+        <p style={{color: "white" } }> Esta es mi primera aplicacion en react</p>
         </div>
       
     )
